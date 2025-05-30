755c1bcb0ff9aaf5fc0ea788667ceb51f6fc0e08
Nayelis Nin <nayelisninb.12@gmail.com>
2025-05-30 11:28:28 -0400

    nuevo

--- nosee/nosee.py
+++ nosee/nosee.py
@@ -3,47 +3,58 @@
 import reflex as rx
 
 def navbar():
+    
     return rx.hstack(
-        rx.text("Portals", color="white", font_weight="bold", font_size="5"),
+        rx.vstack(
         rx.spacer(),
+        rx.text("*connect", font_size="1em"),
+        rx.color_mode.button(position="top-right"),
+        ),
+
+
         rx.hstack(
-            rx.text("Home", color="white"),
-            rx.text("Products", color="white"),
-            rx.text("Developers", color="white"),
-            rx.text("Contact", color="white"),
-            spacing="8",
+           rx.spacer(),
+           rx.spacer(),
+           rx.spacer(),
+           rx.spacer(),
+           rx.spacer(),
+           rx.spacer(),
+           rx.spacer(),
+            rx.text("Solutions"),
+            rx.text("Partnership"),
+            rx.text("Pricing"),
+            rx.text("Contact"),
+            spacing="9",
+            align="center",
+            
         ),
-        rx.spacer(),
-        rx.button(
-            "Book a demo",
-            background_color="#6f2db6",
-            color="white",
-            border_radius="8px",
-            padding_x="4",
-            padding_y="2",
-        ),
-        padding="6",
-        background_color="black",
-        width="100%",
-        align_items="center"
-    )
+
+    ),
+        
+       
+        
 
 def hero_section():
     return rx.center(
         rx.vstack(
-            rx.heading("Unleash your creativity", color="white", font_size="9", font_weight="bold"),
+            rx.spacer(),
+            rx.spacer(),
+            rx.spacer(),
+            rx.spacer(),
+            rx.spacer(),
+            rx.spacer(),
+            rx.heading("Unleash your creativity whith plattaform", font_size="9", font_weight="bold", align="center", marging_left="5em"),
             rx.text(
-                "Design, share, and express your ideas freely with our tools.",
-                color="white",
+                "Design, share, and express your ideas freely with our tools, you can create unique content than can be edited and modified. A fun and engaging experience for you and your audience.",
                 font_size="5",
                 max_width="30em",
                 text_align="center"
             ),
             rx.button(
                 "Get Started",
-                background_color="white",
-                color="black",
+
                 border_radius="999px",
+                bg="green",
                 padding_x="6",
                 padding_y="4"
             ),
@@ -52,22 +63,19 @@ def hero_section():
         ),
         padding_y="16",
         position="absolute",
-        top=0,
+        top=7,
         left=0,
         right=0,
         z_index="1"
     )
 
+
+
+    
+
 def index():
     return rx.box(
-        rx.image(
-            src="/fondeo.png",
-            position="absolute",
-            width="100%",
-            height="100%",
-            object_fit="cover",
-            z_index="0"
-        ),
+        
         rx.box(
             navbar(),
             hero_section(),
